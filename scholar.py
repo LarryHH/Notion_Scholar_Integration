e5dc0e8cf549f9bbe54730e23f47c433afb12e41
Larry <larryhieuhuynh@gmail.com>
2023-03-14 21:08:20 +0800

    refactor into class format

--- scholar.py
+++ scholar.py
@@ -1,27 +1,36 @@
 from scholarly import scholarly
 
-def get_single_pub_by_title(title):
-    """get single Scholar result from title search"""
-    search_query = None
-    try:
-        search_query = scholarly.search_single_pub(title, filled = False)
-    except IndexError as err:
-        print(err)
-    if search_query:
-        name = search_query['bib'].get('title', None)
-        authors = search_query['bib'].get('author', None)
-        year = search_query['bib'].get('pub_year', None)
-        venue = search_query['bib'].get('venue', None)
-        link = search_query.get('eprint_url', None)
-        citations = search_query.get('num_citations', None)
+class Source:
+    """single Google Soholar search result"""
+    def __init__(self) -> None:
+        self.name = None
+        self.authors = None
+        self.year = None
+        self.venue = None
+        self.link = None
+        self.citations = None
 
-        print(name)
-        print(authors)
-        print(year)
-        print(venue)
-        print(link)
-        print(citations)
+    def __str__(self) -> str:
+        return f'{self.name},{self.authors},{self.year},{self.citations},{self.venue},{self.link}'
+
+    def get_pub_by_title(self, title):
+        """get single Scholar result from title search"""
+        search_query = None
+        try:
+            search_query = scholarly.search_single_pub(title, filled = False)
+        except IndexError as err:
+            print(err)
+        if search_query:
+            self.name = str(search_query['bib'].get('title', None))
+            self.authors = list(search_query['bib'].get('author', None))
+            self.year = int(search_query['bib'].get('pub_year', None))
+            self.venue = str(search_query['bib'].get('venue', None))
+            self.link = str(search_query.get('eprint_url', None))
+            self.citations = int(search_query.get('num_citations', None))
+    
 
 if __name__ == "__main__":
-    get_single_pub_by_title('Perception of physical stability and center of mass of 3D objects')
+    source = Source()
+    source.get_pub_by_title('V. Giovannetti, S. Lloyd, and L. Maccone, “Quantum random access memory,” Physical Review Letters, April 2008.')
+    print(source)
 
